519118724a6c76e97fb1f2050f339ac16d84a1bc
Nguyen Van Dat <datsunbae@gmail.com>
2022-08-07 18:14:58 +0700

    Route get all characters

--- index.js
+++ index.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const cheerio = require('cheerio');
+const axios = require('axios')
 const dotenv = require('dotenv');
 const port = 3000;
-
+const urlAllCharacter = 'https://onepiece.fandom.com/wiki/List_of_Canon_Characters';
 const app = express();
 
-app.get('/', (req, res) => {
-    res.status(200).json('Naruto Api');
+//SET UP PROJECT
+dotenv.config();
+
+//ROUTES 
+
+//GET ALL CHARACTERS
+app.get('/all_characters', (req, res) => {
+    const obj = [];
+    const limit = Number(req.query.limit);
+    try{
+        axios(urlAllCharacter)
+            .then(response => {
+                const html = response.data;
+                const $ = cheerio.load(html);
+                $('.wikitable > tbody > tr > td:nth-child(2)', html).each(function() {
+                    let name = $(this).find("a").attr("title");
+                    let url = $(this).find("a").attr("href");
+                    obj.push({
+                        name: name,
+                        url: "http://localhost:3000/all_characters" + url.split('/wiki')[1]
+                    })
+                })
+
+                if(limit && limit > 0){
+                    res.status(200).json(obj.slice(0, limit));
+                    return;
+                }
+                res.status(200).json(obj);
+                
+            })
+    }catch(err){
+        console.log(err)
+        res.status(500).json(err);
+    }
 })
+
+//GET A CHARACTERS
+
+
 
 app.listen(port, () => {
     console.log('Server is running...');
