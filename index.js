b281b7562a5ef23de6c0b313f083859e040a8dda
Nguyen Van Dat <datsunbae@gmail.com>
2022-08-08 22:56:13 +0700

    Route get a character details

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const axios = require('axios')
 const dotenv = require('dotenv');
 const port = 3000;
 const urlAllCharacter = 'https://onepiece.fandom.com/wiki/List_of_Canon_Characters';
+const urlCharacter = 'https://onepiece.fandom.com/wiki/';
 const app = express();
 
 //SET UP PROJECT
@@ -20,7 +21,7 @@ app.get('/all_characters', (req, res) => {
             .then(response => {
                 const html = response.data;
                 const $ = cheerio.load(html);
-                $('.wikitable > tbody > tr > td:nth-child(2)', html).each(function() {
+                $('table >  tbody > tr > td:nth-child(2)', html).each(function() {
                     let name = $(this).find("a").attr("title");
                     let url = $(this).find("a").attr("href");
                     obj.push({
@@ -43,7 +44,51 @@ app.get('/all_characters', (req, res) => {
 })
 
 //GET A CHARACTERS
+app.use('/all_characters/:character', (req, res) => {
+    let image = '';
+    const titles = [];
+    const details = [];
+    const characterObjects = {};
+    try {
+        axios(urlCharacter + req.params.character)
+            .then(response => {
+                const html = response.data;
+                const $ = cheerio.load(html);
+                $('aside > .pi-image-collection > .wds-is-current', html).each(function() {
+                    image = $(this).find('img').attr('src');
+                })
 
+                $('aside', html).each(function() {
+                    //Get character images
+                    $(this).find('.pi-image-collection > .wds-is-current > img').each(function() {
+                        images.push($(this).attr('src'));
+                    });
+
+                    //Get character titles
+                    $(this).find('aside > section > div > h3').each(function() {
+                        titles.push($(this).text().toLowerCase().replace(':', ''));
+                    })
+
+                    //Get charactor details
+                    $(this).find('aside > section > div > .pi-data-value').each(function() {
+                        details.push($(this).text());
+                    })
+
+                });
+
+                for(let i = 0; i < titles.length; i++){
+                    characterObjects[titles[i]] = details[i];
+                }
+                const newCharacterObjects = {
+                    image,
+                    ...characterObjects
+                }
+                res.status(200).json(newCharacterObjects);
+            })
+    }catch (err){
+        res.status(500).json(err);
+    }
+})
 
 
 app.listen(port, () => {
